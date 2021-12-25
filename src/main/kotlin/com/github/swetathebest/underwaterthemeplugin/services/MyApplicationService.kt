92e582917cb6f5ecd9eaba8c0e4d4d5158b6eae7
GitHub Action <action@github.com>
2021-12-25 11:54:39 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/swetathebest/underwaterthemeplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.swetathebest.underwaterthemeplugin.services
+
+import com.github.swetathebest.underwaterthemeplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/swetathebest/underwaterthemeplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/swetathebest/underwaterthemeplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.swetathebest.underwaterthemeplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.swetathebest.underwaterthemeplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
